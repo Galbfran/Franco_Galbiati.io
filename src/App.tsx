dd8f82cc8b6f52682efb6edc4821461c804af70c
Galbfran <francogalbiati984@gmail.com>
2023-07-27 11:10:59 -0300

    subo cambio

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Header from './componentes/Header'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Inicio from './componentes/Inicio/Inicio'
-import Calculadora from './componentes/Calculadora/Calculadora'
+
 
 function App() {
 
